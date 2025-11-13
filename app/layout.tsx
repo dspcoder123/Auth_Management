082bc82580ca6023bc395633d0ab55d144bc1aa2
dspcoder123 <dspcider123@gmail.com>
2025-11-13 15:47:52 +0530

    removed window function

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,22 +16,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-70QEE0P49H"
-          strategy="afterInteractive"
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-70QEE0P49H');
-            `,
-          }}
-        />
       </head>
       <body>
         
